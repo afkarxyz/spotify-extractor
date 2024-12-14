df5ab656239ee61d4568a17e71816e6554e9b508
afkarxyz <mzamzamafkarhadiq@gmail.com>
2024-12-14 13:05:21 +0700

    Update album.py

--- album.py
+++ album.py
@@ -1,9 +1,9 @@
 import json
-from spotapi import PublicAlbum
+from spotapi.public import Public
 
 def get_album_raw_json(album_url):
-    album = PublicAlbum(album_url)
-    return album.get_album_info()
+    album_id = album_url.split('/')[-1]
+    return next(Public.album_info(album_id))
 
 album_url = "https://open.spotify.com/album/4VZ7jhV0wHpoNPCB7Vmiml"
 raw_json = get_album_raw_json(album_url)
