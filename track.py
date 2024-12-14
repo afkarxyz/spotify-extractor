c8b50d8b8c9021b2ac81d555a5988109a135b438
afkarxyz <mzamzamafkarhadiq@gmail.com>
2024-12-14 13:24:10 +0700

    Update track.py

--- track.py
+++ track.py
@@ -1,10 +1,6 @@
-import json
 from spotapi.public import Public
+import json
 
-def get_track_raw_json(track_url):
-    track_id = track_url.split('/')[-1]
-    return Public.song_info(track_id)
-
-track_url = "https://open.spotify.com/track/2plbrEY59IikOBgBGLjaoe"
-raw_json = get_track_raw_json(track_url)
-print(json.dumps(raw_json, indent=2))
+track_id = "2plbrEY59IikOBgBGLjaoe"
+track_info = Public.song_info(track_id)
+print(json.dumps(track_info, indent=2))
