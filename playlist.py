4a07bdff90ffb86a6248e3ead9f57e6b2c4d6e9c
afkarxyz <mzamzamafkarhadiq@gmail.com>
2024-12-14 13:05:31 +0700

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -1,9 +1,9 @@
 import json
-from spotapi import PublicPlaylist
+from spotapi.public import Public
 
 def get_playlist_raw_json(playlist_url):
-    playlist = PublicPlaylist(playlist_url)
-    return playlist.get_playlist_info()
+    playlist_id = playlist_url.split('/')[-1]
+    return next(Public.playlist_info(playlist_id))
 
 playlist_url = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF"
 raw_json = get_playlist_raw_json(playlist_url)
